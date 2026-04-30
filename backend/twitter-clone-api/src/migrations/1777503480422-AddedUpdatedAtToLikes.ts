4133c1e13b04c6dae363e019347555392c05e3de
gooddogdevelopment-sys <gooddogdevelopment@gmail.com>
2026-04-29 20:01:39 -0400

    Lint

--- backend/twitter-clone-api/src/migrations/1777503480422-AddedUpdatedAtToLikes.ts
+++ backend/twitter-clone-api/src/migrations/1777503480422-AddedUpdatedAtToLikes.ts
@@ -1,18 +1,27 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddedUpdatedAtToLikes1777503480422 implements MigrationInterface {
-    name = 'AddedUpdatedAtToLikes1777503480422'
+  name = 'AddedUpdatedAtToLikes1777503480422';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "likes" DROP CONSTRAINT "UQ_likes_userId_postId"`);
-        await queryRunner.query(`ALTER TABLE "likes" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "likes" ADD CONSTRAINT "UQ_74b9b8cd79a1014e50135f266fe" UNIQUE ("userId", "postId")`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "likes" DROP CONSTRAINT "UQ_likes_userId_postId"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "likes" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "likes" ADD CONSTRAINT "UQ_74b9b8cd79a1014e50135f266fe" UNIQUE ("userId", "postId")`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "likes" DROP CONSTRAINT "UQ_74b9b8cd79a1014e50135f266fe"`);
-        await queryRunner.query(`ALTER TABLE "likes" DROP COLUMN "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "likes" ADD CONSTRAINT "UQ_likes_userId_postId" UNIQUE ("userId", "postId")`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "likes" DROP CONSTRAINT "UQ_74b9b8cd79a1014e50135f266fe"`,
+    );
+    await queryRunner.query(`ALTER TABLE "likes" DROP COLUMN "updatedAt"`);
+    await queryRunner.query(
+      `ALTER TABLE "likes" ADD CONSTRAINT "UQ_likes_userId_postId" UNIQUE ("userId", "postId")`,
+    );
+  }
 }
