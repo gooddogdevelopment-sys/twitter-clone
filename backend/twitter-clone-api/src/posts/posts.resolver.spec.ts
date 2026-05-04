df8fd73d5273961ccb935a41b15054918150a3af
gooddogdevelopment-sys <gooddogdevelopment@gmail.com>
2026-05-04 12:28:42 -0400

    Updated posts tests to run successfully

--- backend/twitter-clone-api/src/posts/posts.resolver.spec.ts
+++ backend/twitter-clone-api/src/posts/posts.resolver.spec.ts
@@ -1,16 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { PostsResolver } from './posts.resolver';
 import { PostsService } from './posts.service';
+import { Post } from './entities/post.entity';
+import { Follower } from '../followers/entites/followers.entity';
+import { Reposts } from '../repost/entities/reposts.entity';
+import { UsersService } from '../users/users.service';
+import { LikesService } from '../likes/likes.service';
+import { RepostsService } from '../repost/reposts.service';
+import { CommentsService } from '../comments/comments.service';
+
+const mockPostsRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findBy: jest.fn(),
+  findOneBy: jest.fn(),
+  preload: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockFollowersRepository = { find: jest.fn() };
+const mockRepostsRepository = { find: jest.fn() };
+
+const mockUsersService = {
+  findByClerkId: jest.fn(),
+  findById: jest.fn(),
+};
+
+const mockLikesService = {
+  countLikes: jest.fn(),
+  isLikedByCurrentUser: jest.fn(),
+};
+
+const mockRepostsService = {
+  countReposts: jest.fn(),
+  isRepostedByCurrentUser: jest.fn(),
+};
+
+const mockCommentsService = {
+  countComments: jest.fn(),
+};
 
 describe('PostsResolver', () => {
   let resolver: PostsResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsResolver, PostsService],
+      providers: [
+        PostsResolver,
+        PostsService,
+        { provide: getRepositoryToken(Post), useValue: mockPostsRepository },
+        {
+          provide: getRepositoryToken(Follower),
+          useValue: mockFollowersRepository,
+        },
+        {
+          provide: getRepositoryToken(Reposts),
+          useValue: mockRepostsRepository,
+        },
+        { provide: UsersService, useValue: mockUsersService },
+        { provide: LikesService, useValue: mockLikesService },
+        { provide: RepostsService, useValue: mockRepostsService },
+        { provide: CommentsService, useValue: mockCommentsService },
+      ],
     }).compile();
 
     resolver = module.get<PostsResolver>(PostsResolver);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
--- backend/twitter-clone-api/src/posts/posts.service.spec.ts
+++ backend/twitter-clone-api/src/posts/posts.service.spec.ts
@@ -1,15 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { PostsService } from './posts.service';
+import { Post } from './entities/post.entity';
+import { Follower } from '../followers/entites/followers.entity';
+import { Reposts } from '../repost/entities/reposts.entity';
+import { UsersService } from '../users/users.service';
+
+const mockPostsRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+  find: jest.fn(),
+  findBy: jest.fn(),
+  findOneBy: jest.fn(),
+  preload: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockFollowersRepository = {
+  find: jest.fn(),
+};
+
+const mockRepostsRepository = {
+  find: jest.fn(),
+};
+
+const mockUsersService = {
+  findByClerkId: jest.fn(),
+  findById: jest.fn(),
+};
 
 describe('PostsService', () => {
   let service: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        { provide: getRepositoryToken(Post), useValue: mockPostsRepository },
+        {
+          provide: getRepositoryToken(Follower),
+          useValue: mockFollowersRepository,
+        },
+        {
+          provide: getRepositoryToken(Reposts),
+          useValue: mockRepostsRepository,
+        },
+        { provide: UsersService, useValue: mockUsersService },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
